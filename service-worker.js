b229b3721e6e7ce237906207fd0e4e2fdcd3e247
andrechya <95126199+andrechya@users.noreply.github.com>
2025-02-18 01:36:07 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,34 @@
 const cacheName = "offline-cache-v1";
 const filesToCache = [
-  "/",
-  "/index.html",
-  "/favicon.ico",
-  "/manifest.json"
+  "./",
+  "./index.html",
+  "./css/bootstrapMin.css",
+  "./js/sweetalert2@11.js",
+  "./favicon.ico",
+  "./icon.png",
+  "./manifest.json",
+  "./notif.mp3"
 ];
 
-// Install Service Worker
+// Install Service Worker & Cache Resources
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(filesToCache);
+    caches.open(cacheName).then(async (cache) => {
+      try {
+        await cache.addAll(filesToCache);
+        console.log("✅ Semua file berhasil di-cache!");
+      } catch (error) {
+        console.error("❌ Cache gagal:", error);
+      }
     })
   );
 });
 
-// Fetch Resources
+// Fetch Request
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
-    })
+    }).catch(() => caches.match("./index.html")) // Jika offline, buka index.html
   );
 });
